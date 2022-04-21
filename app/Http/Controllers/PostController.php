a8905901706687e8ed500a9cc7e5ee6fa6fd386c
andijayawizard <andijayawizard@gmail.com>
2022-04-21 13:35:01 +0700

    2104

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::latest()->paginate(5);
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan!']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $this->validate($request, [
+            'image' => 'image|mimes:png,jpg,jpeg,gif,svg|max:2048',
+            "title" => 'required|min:5',
+            'content' => 'required|min:10'
+        ]);
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+            Storage::delete('public/posts' . $post->image);
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        } else {
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        }
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil diubah']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        Storage::delete('public/posts' . $post->image);
+        $post->delete();
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil dihapus']);
+    }
+}
